73c94d8b1794aa1b71ceef8213a5de638765a4eb
Sahapat <sahapat.starcaft@gmail.com>
2019-01-20 11:52:52 +0700

    Create UDPClient with loop back ip

---
+++ UDPClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String server = "127.0.0.1";
+            int serverPort = 5000;
+            String msg = "Sahapat tong-on ___ 5901292";
+            byte[] sentPackage = Encoding.ASCII.GetBytes(msg);
+            UdpClient client = new UdpClient();
+
+            try
+            {
+                client.Send(sentPackage,sentPackage.Length,server,serverPort);
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine(e.ErrorCode + " " + e.Message);
+            }
+        }
+    }
+}
