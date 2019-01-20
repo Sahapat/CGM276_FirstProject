aa6ea29ee5640281ec65f4f1308b6d6b85fa2d68
Sahapat <sahapat.starcaft@gmail.com>
2019-01-20 11:54:05 +0700

    Create UDP server for receive message

---
+++ UDPServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int serverPort = 5000;
+            UdpClient client = null;
+
+            client = new UdpClient(serverPort);
+            IPEndPoint m_ipEndPoint = new IPEndPoint(IPAddress.Any,0);
+            
+            while(true)
+            {
+                try
+                {
+                    byte[] buffer = client.Receive(ref m_ipEndPoint);
+                    String revcMsg = Encoding.ASCII.GetString(buffer,0,buffer.Length);
+                    Console.WriteLine(revcMsg);
+                }
+                catch(SocketException e)
+                {
+                    Console.WriteLine(e.ErrorCode + " "+e.Message);
+                }
+            }
+        }
+    }
+}
